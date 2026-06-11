0a6ab7a3790d4ccf2eefde4072bf4161e6ee8b32
khushalshrimal <khushalshrimal@gmail.com>
2026-06-11 17:10:06 +0530

    landing page

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Flask!"
+    return render_template("landing.html")
+
+@app.route("/upload")
+def upload():
+    return "<h1>Upload Resume Page</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
